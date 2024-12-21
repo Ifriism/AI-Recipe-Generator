43f60b7526420494a407a59836d4ac8688ae4ba9
Ifriism <idafm2@hotmail.com>
2024-12-21 14:57:44 +0100

    Updated the code to improve the UX

--- src/index.js
+++ src/index.js
@@ -12,13 +12,13 @@ function recipeGenerator(event) {
 
     let instructionsInput = document.querySelector("#user-instructions");
       let apiKey = "fea6579f5ctf53bb7491ae80ac32o60f";
-      let context = "You're an expert in cooking and enjoys coming up with recipes. Please generate recipes that are easy to read, separated into appropriate sections i.e. cooking time, ingredients, instructions and separating these sections with a <br />. Please make sure to follow the user instructions";
+      let context = "You're an expert in cooking and enjoys coming up with recipes. Please generate recipes that are easy to read. Please generate a headline with a name for the recipe and add a <br /> after the name. Please create separate sections for the amount of people the recipe serves, the cooking time, the ingredients, and the instructions and separate them with a <br /> so there is space between each section. The section titles should be in a <strong />. The instructions section should have each step listed in a numeral <li> format. The ingredients section should have each step listed in a <li> format. Please make sure to follow the user instructions";
       let prompt = `User instructions: Generate a recipe using ${instructionsInput.value}`;
       let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
      
 let poemElement = document.querySelector("#recipe");
 poemElement.classList.remove("hidden");
-poemElement.innerHTML = `<div class="generating">Generating recipe ${instructionsInput.value}</div>`;
+poemElement.innerHTML = `<div class="generating">Generating recipe with ${instructionsInput.value}</div>`;
 
       axios.get(apiUrl).then(generatePoem);
 
